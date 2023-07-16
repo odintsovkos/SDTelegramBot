407c0e954e6e1611112281b669b900e4d422449d
Konstantin <kos5172@yandex.ru>
2023-07-16 18:44:54 +0300

    - Fix

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -23,6 +23,9 @@ async def bot_help(message: types.Message):
     text = ("Список команд: ",
             "/start - Начать генерировать",
             "/settings - Настройки генерации",
-            "/help - Справка")
+            "/help - Справка",
+            "Поблагодарить автора:",
+            "Qiwi: <code>+79803778005</code>",
+            "Карта МИР: <code>2200 1509 9863 3876</code>",)
 
     await message.answer("\n".join(text))
